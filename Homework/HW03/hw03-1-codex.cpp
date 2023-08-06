2e88763c4be069ba3e4b3321ea924b8402fe5d42
wachilanee <wachilanee47@gmail.com>
2023-08-06 15:27:11 +0700

    hw3-1

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -5,14 +5,14 @@
 
 int main() {
   int a = 10, b = 20, c = 30, d = 40 ; 
-  printf( "This is %d + %d = %d", a, b, a+b ) ;
+  printf( "This is %d + %d = %d",a, b, a+b ) ;
   for( int i = 0; i < d - 30; i++ ) {
-      if( i < 30 ){
+      if( i < 30 ) {
           printf( "Here" ) ;
           if( i == 8 ) {
           printf( "This is 8." ) ;
-      }
-    }
+          }
+        }
     printf( "ok" ) ;
   }
   return 0 ;
